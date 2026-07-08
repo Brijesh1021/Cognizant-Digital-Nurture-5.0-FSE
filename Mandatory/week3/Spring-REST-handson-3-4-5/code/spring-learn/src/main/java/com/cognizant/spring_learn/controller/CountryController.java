d287a825a046105b620fe09ba8868a451fe4d1dc
M.Brijesh <brijesh@Mac.lan>
2026-07-08 22:43:25 +0530

    handson5

--- Mandatory/week3/Spring-REST-handson-3-4-5/code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ Mandatory/week3/Spring-REST-handson-3-4-5/code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -7,12 +7,27 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cognizant.spring_learn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 public class CountryController {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(CountryController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
+
+    }  
 
     @RequestMapping("/country")
     public Country getCountryIndia() {
